9f0ab95181ca3b75e417e778acecc274cb161636
Christopher Joseph <123834992+ChrisxCross2003@users.noreply.github.com>
2025-07-01 12:54:31 -0400

    Delete Course Review App directory

--- Course Review App/src/main/java/edu/virginia/cs/reviews/Main.java
+++
@@ -1,9 +0,0 @@
-package edu.virginia.cs.reviews;
-
-public class Main {
-    public static void main(String[] args) {
-        DataLayer dl = new DataLayer();
-        dl.connect();
-        //dl.printData();
-    }
-}
